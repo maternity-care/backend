e69458bcea6888922aa5a843175684d3e9d05bcc
xJunoo <duyvthe180740@fpt.edu.vn>
2026-08-24 23:29:15 +0700

    Add realtime appointment notifications

--- src/modules/appointments/appointments.module.ts
+++ src/modules/appointments/appointments.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FacilityService } from '../facility-services/entities/facility-service.entity';
 import { DoctorShift } from '../shifts/entities/shift.entity';
 import { SchedulesModule } from '../schedules/schedules.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { RealtimeModule } from '../realtime/realtime.module';
 import { AppointmentsController } from './appointments.controller';
 import { ManagementAppointmentsController } from './management-appointments.controller';
 import { AppointmentsService } from './appointments.service';
@@ -13,6 +15,8 @@ import { AppointmentServiceItem } from './entities/appointment-service-item.enti
   imports: [
     TypeOrmModule.forFeature([Appointment, AppointmentServiceItem, FacilityService, DoctorShift]),
     SchedulesModule,
+    NotificationsModule,
+    RealtimeModule,
   ],
   controllers: [AppointmentsController, ManagementAppointmentsController],
   providers: [AppointmentsService],
